a8567aa62ba40da91f8afa7d3204fa6e7f639c2f
Agrati <filipeagrati@gmail.com>
2017-09-19 12:01:56 +0100

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { State, process } from '@progress/kendo-data-query';
+
+import { Product } from './model';
+import { EditService } from './edit.service';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  cars = [
+    {color: 'red', brand: 'BMW', name: 'Cobra', year: 2005},
+    {color: 'blue', brand: 'Porche', name: 'Cabrero', year: 2015},
+    {color: 'black', brand: 'Mercedes', name: 'E41', year: 2009},
+    {color: 'yellow', brand: 'Ferrari', name: 'Fer', year: 2017}
+  ];
+
+  view: Observable<GridDataResult>;
+
+  public gridState: State = {
+      sort: [],
+      skip: 0,
+      take: 10
+  };
+
+  private editService: EditService;
+  private editedRowIndex: number;
+  private editedProduct: Product;
+
+  gridView: any[] = products;
+
+  constructor(@Inject(EditService) editServiceFactory: any) {
+      this.editService = editServiceFactory();
+  }
+
+  ngOnInit(): void {
+       this.view = this.editService.map(data => process(data, this.gridState));
+
+      this.editService.read();
+  }
+
+  public onStateChange(state: State) {
+      this.gridState = state;
+
+      this.editService.read();
+  }
+
+  protected addHandler({sender}) {
+      this.closeEditor(sender);
+
+      sender.addRow(new Product());
+  }
+
+  protected editHandler({sender, rowIndex, dataItem}) {
+      this.closeEditor(sender);
+
+      this.editedRowIndex = rowIndex;
+      this.editedProduct = Object.assign({}, dataItem);
+
+      sender.editRow(rowIndex);
+  }
+
+  protected cancelHandler({sender, rowIndex}) {
+      this.closeEditor(sender, rowIndex);
+  }
+
+  private closeEditor(grid, rowIndex = this.editedRowIndex) {
+      grid.closeRow(rowIndex);
+      this.editService.resetItem(this.editedProduct);
+      this.editedRowIndex = undefined;
+      this.editedProduct = undefined;
+  }
+
+  protected saveHandler({sender, rowIndex, dataItem, isNew}) {
+      this.editService.save(dataItem, isNew);
+
+      sender.closeRow(rowIndex);
+
+      this.editedRowIndex = undefined;
+      this.editedProduct = undefined;
+  }
+
+  protected removeHandler({dataItem}) {
+      this.editService.remove(dataItem);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Jsonp, JsonpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
+import { EditService } from './edit.service';
+import { DataTableModule } from 'primeng/primeng';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    JsonpModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    GridModule,
+    DataTableModule
   ],
-  providers: [],
+  providers: [
+    {
+      deps: [Jsonp],
+      provide: EditService,
+      useFactory: (jsonp: Jsonp) => () => new EditService(jsonp)
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
